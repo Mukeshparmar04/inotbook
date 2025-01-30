668535fdc87aad79edc148d25af15bd5ba7586c5
Mukeshparmar04 <mukeshpiploda2002@gmail.com>
2025-01-31 00:08:44 +0530

    Context api added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { Home } from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 import {
    BrowserRouter as Router,
    Routes, 
@@ -11,6 +12,7 @@ import {
 function App() {
   return (
     <>
+    <NoteState>
       <Router>
         <Navbar />
         <Routes>
@@ -18,6 +20,7 @@ function App() {
           <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
+    </NoteState>
     </>
   );
 }
