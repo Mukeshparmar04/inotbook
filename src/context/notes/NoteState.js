242512cc0faf22f832708aed2967cddd0f8babbb
Mukeshparmar04 <mukeshpiploda2002@gmail.com>
2025-02-02 23:24:43 +0530

    Adding and Deleting Node in Data base

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -34,6 +34,9 @@ const NoteState = (props)=>{
             },
             body:JSON.stringify({title, description, tag})
         });
+
+        const json = await response.json();
+        console.log(json)
         
         // Logic to Add in Client site
         const note = {
@@ -72,24 +75,28 @@ const NoteState = (props)=>{
     const editNote = async (id, title, description, tag) =>{
         // API Call
         const response = await fetch(`${host}/api/notes/updatenote/${id}`,{
-            method: "POST",
+            method: "PUT",
             headers: {
                 'Content-Type': 'application/json',
                 'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjc5OWZkOTExNDk5ZmRlYjExMTYxZDU0In0sImlhdCI6MTczODE0NTI5NH0.MVMdIuhyp5YarpuuZFTfN_1bbxfVkdXtAJvN8tV4xuw',
             },
             body:JSON.stringify({title, description, tag})
         });
-        const json = response.json();
+        const json = await response.json();
+        console.log(json)
 
+        let newNotes = JSON.parse(JSON.stringify(notes))
         // Logic to edit in Client site
-        for (let index = 0; index < notes.length; index++) {
-            const element = notes[index];
+        for (let index = 0; index < newNotes.length; index++) {
+            const element = newNotes[index];
             if(element._id === id){
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                break;
             }
         }
+        setNotes(newNotes);
     }
 
 
